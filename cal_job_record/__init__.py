dba44aa166e07b574eeb5745967391d380784347
chendepeng <chendepeng@bjoberj.wecom.work>
2023-03-24 11:01:02 +0800

    计算任务的状态、修改参数

--- cal_job_record/__init__.py
+++ cal_job_record/__init__.py
@@ -2,16 +2,16 @@ JobEtcdRecord = None
 JobFileRecord = None
 job_etcd_restart = None
 rm_run_info = None
-# try:
-#     from .etcd_jobRecord import JobEtcdRecord, job_etcd_restart
-# except Exception:
-#     pass
+try:
+    from .etcd_jobRecord import JobEtcdRecord, job_etcd_restart
+except Exception:
+    pass
 try:
     from .file_jobRecord import JobFileRecord, rm_run_info
 except Exception:
     pass
 
 JOBRecord = JobEtcdRecord or JobFileRecord
-rm_run_info = job_etcd_restart or rm_run_info
-if JOBRecord is None:
+JOBRecordInfoRestart = job_etcd_restart or rm_run_info
+if JOBRecord is None or JOBRecordInfoRestart is None:
     raise Exception("丢失任务记录模块")
