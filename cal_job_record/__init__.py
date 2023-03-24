6660f7eec96ce524b24cf7be19b07559ebfe4a0e
chendepeng <chendepeng@bjoberj.wecom.work>
2023-03-24 09:15:09 +0800

    计算的状态和参数

--- cal_job_record/__init__.py
+++ cal_job_record/__init__.py
@@ -2,10 +2,10 @@ JobEtcdRecord = None
 JobFileRecord = None
 job_etcd_restart = None
 rm_run_info = None
-try:
-    from .etcd_jobRecord import JobEtcdRecord, job_etcd_restart
-except Exception:
-    pass
+# try:
+#     from .etcd_jobRecord import JobEtcdRecord, job_etcd_restart
+# except Exception:
+#     pass
 try:
     from .file_jobRecord import JobFileRecord, rm_run_info
 except Exception:
