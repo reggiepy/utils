88f51963af1deedc4e82bcc830c9aab18ac4536e
root <wangtongpy@foxmail.com>
2023-03-29 11:22:41 +0800

    use pyqt

--- pyqt_utils.py
+++ pyqt_utils.py
@@ -7,11 +7,22 @@ from PyQt5 import QtWidgets
 
 
 class QTextEditLogger(logging.Handler):
-    def __init__(self, parent):
+    def __init__(self, widget: QtWidgets.QPlainTextEdit):
         super().__init__()
-        self.widget = QtWidgets.QPlainTextEdit(parent)
+        self.widget = widget
         self.widget.setReadOnly(True)
 
     def emit(self, record):
         msg = self.format(record)
         self.widget.appendPlainText(msg)
+
+
+class QTextBrowserLogger(logging.Handler):
+    def __init__(self, browser: QtWidgets.QTextBrowser):
+        super().__init__()
+        self.browser = browser
+        self.browser.document().setMaximumBlockCount(1000)
+
+    def emit(self, record):
+        msg = self.format(record)
+        self.browser.append(msg)
